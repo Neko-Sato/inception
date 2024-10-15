32b36dbe88861e72567f223c359b4dbfb6332ce0
hshimizu <hshimizu@student.42tokyo.jp>
2024-10-16 01:43:21 +0900

    update

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,7 @@
+<?php
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'wordpress' );
+define( 'DB_PASSWORD', 'wordpress' );
+define( 'DB_HOST', 'mariadb' );
+
+$table_prefix = 'wp_';
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,11 +1,12 @@
 set -e
 
 if [ ! -d /var/www/html ]; then
-    mkdir -p /var/www/html
-    chown -R www-data:www-data /var/www/html
     curl -fsOL https://ja.wordpress.org/latest-ja.tar.gz
     tar -zxvf latest-ja.tar.gz
-    cp -r wordpress/* /var/www/html
+    rm -f latest-ja.tar.gz
+    mv wordpress /var/www/html
+    mv wp-config.php /var/www/html/
+    chown -R www-data:www-data /var/www/html
 fi
 
 exec php-fpm8.2 -F
