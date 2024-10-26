28edecde222723a56b36fda444a4150af3207751
Neko-Sato <hisatosi35350@gmail.com>
2024-10-27 05:50:43 +0900

    update

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 set -e
 
-WP_PATH=/var/www/wordpress
+WP_PATH="/var/www/wordpress"
 
-gosu www-data sh <<EOF
-wp db check --path="$WP_PATH"
-wp core update-db --path="$WP_PATH"
+wp_exec () {
+  (
+    cd /var/www/wordpress
+    wp --allow-root $@
+  )
+}
 
-if ! wp user get "$WP_USER1_NAME" --field=ID > /dev/null 2>&1; then
-	wp user create "$WP_USER1_NAME" "$WP_USER1_EMAIL" --user_pass="$WP_USER1_PASSWORD" --role=administrator
+if ! wp_exec core is-installed; then
+  wp_exec core install \
+    --url="https://$DOMAIN_NAME" \
+    --title="$WP_BLOG_TITLE" \
+    --admin_user="$WP_USER1_NAME" \
+    --admin_password="$WP_USER1_PASSWORD" \
+    --admin_email="$WP_USER1_EMAIL"
 fi
 
-if ! wp user get "$WP_USER2_NAME" --field=ID > /dev/null 2>&1; then
-	wp user create "$WP_USER2_NAME" "$WP_USER2_EMAIL" --user_pass="$WP_USER2_PASSWORD" --role=subscriber
+if ! wp_exec user get "$WP_USER2_NAME" --field=ID > /dev/null 2>&1; then
+  wp_exec user create \
+    "$WP_USER2_NAME" \
+    "$WP_USER2_EMAIL" \
+    --user_pass="$WP_USER2_PASSWORD" \
+    --role=subscriber
 fi
-
-wp option update blogname "$WP_BLOG_TITLE"
-wp option update admin_email "$WP_USER1_EMAIL"
-wp option update blog_public false
-wp option update siteurl "https://$DOMAIN_NAME"
-EOF
 
 exec "$@" -F
